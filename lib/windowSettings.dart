f987e6996934f03e04764ef1be3d3a22d9856532
Cristovao07 <108178310+Cristovao07@users.noreply.github.com>
2023-11-22 16:15:14 +0000

    Update windowSettings.dart
    update

--- lib/windowSettings.dart
+++ lib/windowSettings.dart
@@ -34,6 +34,28 @@ class windowSettings extends StatefulWidget {
 //----------------------------------------------------------------
 // ignore: camel_case_types
 class State_windowSettings extends State<windowSettings> {
+  bool valNotify1 = true;
+  bool valNotify2 = false;
+  bool valNotify3 = false;
+
+  onChangeFunction1(bool newValue1) {
+    setState(() {
+      valNotify1 = newValue1;
+    });
+  }
+
+  onChangeFunction2(bool newValue2) {
+    setState(() {
+      valNotify2 = newValue2;
+    });
+  }
+
+  onChangeFunction3(bool newValue3) {
+    setState(() {
+      valNotify3 = newValue3;
+    });
+  }
+
   final windowSettings Ref_Window;
   String className = "";
 
@@ -110,18 +132,93 @@ class State_windowSettings extends State<windowSettings> {
             buildAccountOption(context, "Change Password"),
             buildAccountOption(context, "Change Password"),
             buildAccountOption(context, "Change Password"),
-
+            SizedBox(height: 40),
+            Row(
+              children: [
+                Icon(Icons.volume_up_outlined, color: Colors.blue),
+                SizedBox(width: 10),
+                Text("Notificatios", style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold)
+                )
+              ],
+            ),
+            Divider(height: 20, thickness: 1),
+            SizedBox(height: 10),
+            buildNotificationOption("Theme Dark", valNotify1, onChangeFunction1),
+            buildNotificationOption("Theme Dark", valNotify2, onChangeFunction2),
+            buildNotificationOption("Theme Dark", valNotify3, onChangeFunction3),
+            Center(
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+                ),
+                onPressed: () {},
+                child: Text("SIGN OUT", style: TextStyle(
+                    fontSize: 16,
+                    letterSpacing: 2.2,
+                    color: Colors.black
+                ),),
+              ),
+            )
           ],
         )
       ),
+    );
+  }
+
+  Padding buildNotificationOption(String title, bool value, Function onChangeMethod) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title, style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey[600]
+            )),
+            Transform.scale(
+              scale: 0.7,
+              child: CupertinoSwitch(
+                activeColor: Colors.blue,
+                trackColor: Colors.grey,
+                value: value,
+                onChanged: (bool newValue) {
+                  onChangeMethod(newValue);
+                },
+              ),
+            )
+          ],
+        ),
     );
   }
   GestureDetector buildAccountOption(BuildContext context, String title){
     return GestureDetector(
       onTap: () {
         showDialog(context:context, builder: (BuildContext context){
-
-        }
+          return AlertDialog(
+            title: Text(title),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Option 1"),
+                Text("Option 2")
+              ],
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+              },
+                  child: Text("Close")
+              )
+            ],
+          );
+        });
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
