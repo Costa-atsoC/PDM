6b2724a68b8f76f2d4a882a980c3b99879c655b2
rodrigo <rd.silva@ubi.pt>
2023-12-01 20:56:42 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:ubi/common/appTheme.dart';
-import 'package:ubi/screens/windowInitial.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:ubi/screens/windowInitial.dart';
+import 'package:ubi/windowHome.dart';
 
 import 'firebase_options.dart';
 import 'common/Management.dart';
+import 'common/Utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,18 +34,49 @@ void main() async {
   runApp(const MyApp());
 }
 
+Future<bool> checkInternetConnection() async {
+  return await UtilsFlutter.checkInternetConnection();
+}
+
+@override
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
+
 
   @override
   Widget build(BuildContext context) {
     Management appManagement = Management("APP-RideWME");
     appManagement.Load();
-    return MaterialApp(
-      title: 'RideWME',
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: MyHomePage(appManagement, appManagement.GetDefinicao("TITULO_APP", "TITULO_APP ??")),
+
+    return FutureBuilder<bool>(
+      future: checkInternetConnection(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator(); // Loading indicator while checking internet connection
+        } else {
+          Widget initialScreen;
+
+          String logged = appManagement.GetDefinicao("USERNAME", "null");
+
+          //We are checking if the user has internet connection and if the user is logged in or not
+          if (snapshot.hasData && snapshot.data!) {
+            if (logged != "null") {
+              initialScreen = windowHome(appManagement);
+            } else {
+              initialScreen = MyHomePage(appManagement, appManagement.GetDefinicao("TITULO_APP", "TITULO_APP ??"));
+            }
+          } else {
+            initialScreen = MyHomePage(appManagement, appManagement.GetDefinicao("TITULO_APP", "TITULO_APP ??"));
+          }
+
+          return MaterialApp(
+            title: 'RideWME',
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            home: initialScreen,
+          );
+        }
+      },
     );
   }
 }
